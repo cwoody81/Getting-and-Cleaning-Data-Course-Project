dcab018c052b1e153a582b714c8b152ac856520f
Clint woody <clint.woody81@gmail.com>
2022-09-28 22:35:42 -1000

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Getting-and-Cleaning-Data-Course-Project
+
+This repo was created by Clint Woody for submission as the final course project 
+to the Getting and Cleaning data class.
+
+## Dataset
+
+The data was given to us by Coursera at this [link](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip).
+
+## Files
+
+* codebook.Rmd includes the description of variables, data sets used, and transformation
+of data sets into the final tidy data.
+
+* run_analysis.R is the script that executes the 5 requirements outlined in the 
+project assignment.
+  + 1.Merges the training and the test sets to create one data set.
+  + 2.Extracts only the measurements on the mean and standard deviation for each measurement. 
+  + 3.Uses descriptive activity names to name the activities in the data set
+  + 4.Appropriately labels the data set with descriptive variable names. 
+  + 5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+        
+* Tidydata.csv is a file of the final tidy data asked for in question 5.
--- run_analysis.R
+++ run_analysis.R
@@ -18,27 +18,40 @@ x_test<-read_table("./data/UCI HAR Dataset/test/X_test.txt", col_names=F)
 subject_test<-read_table("./data/UCI HAR Dataset/test/subject_test.txt", col_names="subject")
 ##rename the columns of the x data
 colnames(x_test)<-cnames$X2
-##identify the columns with mean or standard deviation values then select and 
-##create a new table of data. Then bind all test data by column
-scol<-grep("mean|std",names(x_test))
-x_test<-select(x_test,all_of(scol))
+##combine test data columns
 test_data<-cbind(subject_test,y_test,x_test)
 
 ##repeat for train data
 y_train<-read_table("./data/UCI HAR Dataset/train/Y_train.txt", col_names="activity")
 x_train<-read_table("./data/UCI HAR Dataset/train/X_train.txt", col_names=F)
-colnames(x_train)<-cnames$X2
 subject_train<-read_table("./data/UCI HAR Dataset/train/subject_train.txt", col_names="subject")
-scol<-grep("mean|std",names(x_train))
-x_train<-select(x_train,all_of(scol))
+colnames(x_train)<-cnames$X2
 train_data<-cbind(subject_train,y_train,x_train)
 
-##row bind all data into new table of total dat
+##row bind all data into new table of total data
 total_data<-rbind(train_data,test_data)
+##identify the columns with mean or standard deviation values then select and 
+##save back to total data. Then bind all test data by column
+scol<-grep("mean|std",names(total_data))
+total_data<-select(total_data,subject,activity,all_of(scol))
+
 ##change the activity numbers back to the actual activity
 total_data$activity<-anames[total_data$activity, 2]
+## change labels to the appropriate descriptive variable names
+names(total_data)<-gsub("Acc", "Acceleration", names(total_data))
+names(total_data)<-gsub("Gyro", "Gyroscopic", names(total_data))
+names(total_data)<-gsub("Mag", "Magnitude", names(total_data))
+names(total_data)<-gsub("^t", "Time", names(total_data))
+names(total_data)<-gsub("^f", "Frequency", names(total_data))
+names(total_data)<-gsub("-mean()", "Mean", names(total_data), ignore.case = TRUE)
+names(total_data)<-gsub("-std()", "STD", names(total_data), ignore.case = TRUE)
+names(total_data)<-gsub("-freq()", "Frequency", names(total_data), ignore.case = TRUE)
+names(total_data)<-gsub("BodyBody", "Body", names(total_data))
+
 ##group data by subject first then by activity
 activitygroups<-group_by(total_data, subject,activity)
 ##average each variable of data by activity
 Q5data<-summarize_all(activitygroups,mean)
+## create csv file of final data.
+write.csv(Q5data, "Tidydata.csv")
 
